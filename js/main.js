ba0224fa447772a2ced4509cdf4858605701c15d
Tim Harvey <timharvey@pop-os.localdomain>
2019-11-23 21:23:44 +0000

    Added JS functionality for the time-of-day welcome message and the quote-of-the-day. Also added a few id tags to HTML elements and finished off the CSS (minus possible animations)

---
+++ js/main.js
@@ -0,0 +1,62 @@
+//UNIVERSAL VARIABLES
+const currentDate = new Date();
+const currentHour = currentDate.getHours();
+
+
+//WELCOME MSG
+const welcomeMsg = document.querySelector("#welcome-msg");
+let greeting;
+
+if (currentHour > 0 && currentHour < 12) {
+	greeting = "Good morning";
+} else if (currentHour >= 12 && currentHour < 18) {
+	greeting = "Good afternoon";
+} else if ((currentHour >= 18 && currentHour <= 24) || currentHour === 0) {
+	greeting = "Good evening";
+} else {
+	greeting = "Hello";
+}
+
+welcomeMsg.innerHTML = greeting + " Gosia!";
+
+
+//COUNTDOWN
+
+
+
+//QUOTE
+
+const quoteTxt = document.querySelector("#quote-txt");
+const quoteSrc = document.querySelector("#quote-src");
+
+/* The two arrays below act as a bank of quotes. The first array holds the quotes, the second array holds the authors at the same indexes. */
+const quoteArray = new Array();
+quoteArray[0] = "Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.";
+quoteArray[1] = "Strive not to be a success, but rather to be of value.";
+quoteArray[2] = "I may not have gone where I intended to go, but I think I have ended up where I needed to be.";
+quoteArray[3] = "A reader lives a thousand lives before he dies. The man who never reads lives only one.";
+quoteArray[4] = "One glance at a book and you hear the voice of another person, perhaps someone dead for 1,000 years. To read is to voyage through time.";
+quoteArray[5] = "Never miss a good chance to shut up.";
+quoteArray[6] = "The scariest moment is always just before you start.";
+quoteArray[7] = "Whether you believe you can do a thing or not, you are right.";
+quoteArray[8] = "Coming back to where you started is not the same as never leaving.";
+quoteArray[9] = "When I ask how old your toddler is, I don't need to hear '27 months.' 'He's two' will do just fine. He's not a cheese. And I didn't really care in the first place.";
+quoteArray[10] = "Don't let circumstances controle you. You change your circumstances.";
+
+const authorArray = new Array();
+authorArray[0] = "Bernard M. Baruch";
+authorArray[1] = "Albert Einstein";
+authorArray[2] = "Douglas Adams";
+authorArray[3] = "George R.R. Martin";
+authorArray[4] = "Carl Sagan";
+authorArray[5] = "Will Rogers";
+authorArray[6] = "Stephen King";
+authorArray[7] = "Henry Ford";
+authorArray[8] = "Terry Pratchett";
+authorArray[9] = "George Carlin";
+authorArray[10] = "Jackie Chan";
+
+const index = Math.floor(Math.random() * quoteArray.length);
+
+quoteTxt.innerHTML = '"' + quoteArray[index] + '"';
+quoteSrc.innerHTML = '- ' + authorArray[index];
